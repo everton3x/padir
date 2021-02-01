7cacfc9619f06a46cbb2fd238e670546fdfcac22
Everton da Rosa <everton3x@gmail.com>
2021-02-01 11:30:09 -0300

    parse_empenho testado

--- test.R
+++ test.R
@@ -1,8 +1,8 @@
 # Arquivo para testes
 devtools::load_all()
-# bal_desp <- parse_bal_desp("C:/Users/Everton/OneDrive/Prefeitura/2020/PAD/2020-10/cm/MES10/BAL_DESP.TXT")
+# bal_desp <- parse_bal_desp("C:/Users/Everton/OneDrive/Prefeitura/2020/PAD/2020-12/pm/MES12/BAL_DESP.TXT")
 # bal_desp
 
-empenho <- parse_empenho("C:/Users/Everton/OneDrive/Prefeitura/2020/PAD/2020-10/cm/MES10/EMPENHO.TXT")
+empenho <- parse_empenho("C:/Users/Everton/OneDrive/Prefeitura/2020/PAD/2020-12/pm/MES12/EMPENHO.TXT")
 empenho
 write.csv2(empenho, 'empenho.csv')
