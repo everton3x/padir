c62ac5ee2723f3a70eab99aaabb08fb431578b47
Everton da Rosa <everton3x@gmail.com>
2021-02-02 14:08:05 -0300

    parse_bal_ver() impelmentado

--- test.R
+++ test.R
@@ -1,6 +1,6 @@
 # Arquivo para testes
 devtools::load_all()
-dados <- parse_receita("C:/Users/Everton/OneDrive/Prefeitura/2020/PAD/2020-12/pm/MES12/RECEITA.TXT")
+dados <- parse_bal_ver("C:/Users/Everton/OneDrive/Prefeitura/2020/PAD/2020-12/pm/MES12/BAL_VER.TXT")
 # dados
 # write.csv2(dados, 'dados.csv')
 
