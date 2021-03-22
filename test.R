31e90e4c2c4053de074901f1b827270bf557bb6a
Everton da Rosa <everton3x@gmail.com>
2021-03-22 09:26:04 -0300

    TEstes com parse_decreto()

--- test.R
+++ test.R
@@ -1,5 +1,5 @@
 # Arquivo para testes
 devtools::load_all()
 
-dados <- parse_decreto("C:/Users/Everton/OneDrive/Prefeitura/2021/PAD/2021-01/pm/MES01/DECRETO.TXT")
+dados <- parse_decreto("C:/Users/Everton/OneDrive/Prefeitura/2021/PAD/2021-02/pm/MES02/DECRETO.TXT")
 # write.csv2(dados, "dados.csv")
